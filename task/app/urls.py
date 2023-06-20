9fa9cbebd79b734697795ff0e56ec21c4a2017eb
Ankit-Somani <ankitrsomani02@gmail.com>
2023-06-20 16:18:28 +0530

    Implemented ModelViewSet

--- task/app/urls.py
+++ task/app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("tasks/<int:pk>", views.TaskViewSet.as_view({'get': 'retrieve'})),
     path("tasks/create", views.TaskViewSet.as_view({'post' : 'create'})),
     path("tasks/update/<int:pk>", views.TaskViewSet.as_view({'patch': 'partial_update'})),
-    path("tasks/delete/<int:pk>", views.TaskViewSet.as_view({'delete': 'destroy'}))
+    path("tasks/delete/<int:pk>", views.TaskViewSet.as_view({'delete': 'destroy'})),
+    path("tasks/complete/<int:pk>", views.TaskViewSet.as_view({'post': 'complete'}))
 ]
--- task/app/views.py
+++ task/app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -11,17 +12,18 @@ def index(request):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
     # to show all tasks ordered by due date
     def list(self, request):
-        queryset = Task.objects.all().order_by('due_date')
-        serializer = TaskSerializer(queryset, many=True)
+        tasks = Task.objects.all().order_by('due_date')
+        serializer = self.get_serializer(tasks, many=True)
         return Response(serializer.data)
     
     # to show incomplete tasks
     def list_remaining(self, request):                              
-        queryset = Task.objects.all().filter(completed=False).order_by('due_date')             
-        serializer = TaskSerializer(queryset, many=True)
+        tasks = Task.objects.all().filter(completed=False).order_by('due_date')             
+        serializer = self.get_serializer(tasks, many=True)
         return Response(serializer.data)
 
     # to show a task using its task_no
@@ -35,11 +37,6 @@ class TaskViewSet(viewsets.ModelViewSet):
                 {'error': 'Task not found.'},
                 status=status.HTTP_404_NOT_FOUND
             )
-
-        # queryset = Task.objects.all()
-        # task = get_object_or_404(queryset, task_no=pk)
-        # serializer = TaskSerializer(task)
-        # return Response(serializer.data)
     
     # to create a task with task_no, task_name, due_date as necessary fields 
     def create(self, request):
@@ -50,42 +47,32 @@ class TaskViewSet(viewsets.ModelViewSet):
         return Response(
             serializer.data, status=status.HTTP_201_CREATED, headers=headers
         )
-        # task_no = request.data['task_no']
-        # task_name = request.data['task_name']
-        # due_date = request.data['due_date']
-        # completed = 0
-        # if('completed' in request.data):
-        #     completed = request.data['completed'] 
-        # if(Task.objects.check(task_no = task_no)):
-        #     return Response("Task number is already taken!")    
-        # task = Task(task_no = task_no, due_date = due_date, task_name = task_name, completed= completed)      
-        # serializer = TaskSerializer(task)
-        # serializer.is_valid(raise_exception=True)
-        # task.save()
-        # return Response(serializer.data)
     
     # to update a task with task_no=pk
     def partial_update(self, request, pk=None):
-        queryset = Task.objects.all()
-        task = get_object_or_404(queryset, task_no=pk)
-        if('task_name' in request.data): 
-            task_name = request.data['task_name']
-            task.task_name = task_name
-        if('completed' in request.data):
-            completed = request.data['completed']
-            task.completed = completed
-        if('due_date' in request.data):
-            due_date = request.data['due_date']
-            task.due_date = due_date  
-        serializer = TaskSerializer(task)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
-        task.save()
+        self.perform_update(serializer)
         return Response(serializer.data)
     
     # to delete a task with task_no = pk
     def destroy(self, request, pk=None):
-        queryset = Task.objects.all()
-        task = get_object_or_404(queryset, task_no=pk)
-        task.delete()
-        return Response("Task %s deleted" %pk)
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    @action(detail=True, methods=['post'])
+    def complete(self, request, pk=None):
+        try:
+            task = self.get_object()
+            task.completed = True
+            task.save()
+            serializer = self.get_serializer(task)
+            return Response(serializer.data)
+        except Task.DoesNotExist:
+            return Response(
+                {'error': 'Task not found.'},
+                status=status.HTTP_404_NOT_FOUND
+            )
     
